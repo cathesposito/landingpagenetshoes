6b2154f57cb34e07e670a0870db9d08d7bbb538a
Catarina Mendes <cata_esposito@hotmail.com>
2021-07-13 20:58:24 -0300

    atualizando style

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const nomeCompleto = document.querySelector("#nomeCompleto");
 const email = document.querySelector('#email');
 
 function armazenaDados(){
+  const bancoDados = [];
   const usuario = {
     "nome": nomeCompleto.value,
     "email": email.value
   };
   localStorage.setItem('usuario', JSON.stringify(usuario));
+  bancoDados.push(usuario);
+  return(
+    window.location.href = "index.html"
+  )
 }
